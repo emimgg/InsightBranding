a9c367938143b82a086d68b0f5e1530511c5a3f6
emimgg <ultralightened@gmail.com>
2024-10-31 09:55:04 -0300

    ""

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import cstmer3 from './assets/customer3.png';
 document.addEventListener('DOMContentLoaded', () => {
     const imgElement = document.querySelector('#logo');
     imgElement.src = logo;
-    
+    imgElement.loading = "lazy"; // Add lazy loading
     const isoImg1 = document.querySelector('#iso1');
     const isoImg2 = document.querySelector('#iso2');
     const heroImage = document.querySelector('#hero-img');
@@ -34,29 +34,41 @@ document.addEventListener('DOMContentLoaded', () => {
     const customer1 = document.querySelector('#customer-1');
     const customer2 = document.querySelector('#customer-2');
     const customer3 = document.querySelector('#customer-3');
+    const teamIconElem = document.querySelector('#teamIconImg');
 
+    // Assign sources and enable lazy loading
     isoImg1.src = iso1;
+    isoImg1.loading = "lazy";
     isoImg2.src = iso2;
+    isoImg2.loading = "lazy";
     heroImage.src = heroImg;
+    heroImage.loading = "lazy";
     brainEle.src = brain;
-    consumerEle.src = consumer;
-    productsEle.src = product;
+    brainEle.loading = "lazy";
     adsEle.src = ads;
+    adsEle.loading = "lazy";
+    consumerEle.src = consumer;
+    consumerEle.loading = "lazy";
+    productsEle.src = product;
+    productsEle.loading = "lazy";
     consultoryEle.src = consultory;
-
+    consultoryEle.loading = "lazy";
+    
     customer1.src = cstmer1;
+    customer1.loading = "lazy";
     customer2.src = cstmer2;
+    customer2.loading = "lazy";
     customer3.src = cstmer3;
+    customer3.loading = "lazy";
 
     starsContainers.forEach(star => {
         star.src = starsImg;
+        star.loading = "lazy";
     });
 
-    const teamIconElem = document.querySelector('#teamIconImg');
-
     teamIconElem.src = teamIcon;
-  });
-
+    teamIconElem.loading = "lazy";
+});
 
 document.getElementById('scrollToTeam').addEventListener('click', function() {
     document.querySelector('.team').scrollIntoView({ behavior: 'smooth' });
